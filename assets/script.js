e5c3c7088c89f36e2850ef9adf286f23d542f41b
Scotty Jirik <ScottyJirik@MacBook-Pro.local>
2023-08-24 10:24:49 -0500

    I think I have the base idea down, now I want to use if thens to remove/add classes

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -15,6 +15,16 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+function timeColor() {
+
+  var timeOfDay = dayjs().hour(); /*this is going to get my current time from day.js*/
+
+  $('.time-block').each(function () {
+    var timeOfBlock = parseInt($(this).attr('id').split('-')[1]) /*this is going to loop my function over all of my time blocks*/
+})
+
+timeColor();
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
