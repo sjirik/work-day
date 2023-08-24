e121c9a5fc394434d9247d4aec4671287f3277c5
Scotty Jirik <ScottyJirik@MacBook-Pro.local>
2023-08-24 11:12:31 -0500

    I added the code to display content that is stored in local storage

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,4 @@
-
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
  
 var button = $('.saveBtn')
 
@@ -15,13 +8,13 @@ button.on('click', function (){
   localStorage.setItem(block, appointment);
 });
 
-
 function blockColor() {
-
+  
   var timeOfDay = dayjs().hour(); /*this is going to get my current time from day.js*/
-
-  $('.time-block').each(function () {
-    var timeOfBlock = parseInt($(this).attr('id').split('-')[1]) /*this is going to loop my function over all of my time blocks*/
+  var timeBlock = $('.time-block')
+  
+  timeBlock.each(function () {
+    var timeOfBlock = parseInt($(this).attr('id').split('-')[1]) /*this is going to loop my function over all of my time blocks and turns them into integers*/
 
     if (timeOfBlock < timeOfDay) {
       $(this).addClass('past');
@@ -41,11 +34,18 @@ function blockColor() {
 
 blockColor();
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  $('#hour-9 .description').val(localStorage.getItem('9'));
+  $('#hour-10 .description').val(localStorage.getItem('10'));
+  $('#hour-11 .description').val(localStorage.getItem('11'));
+  $('#hour-12 .description').val(localStorage.getItem('12'));
+  $('#hour-13 .description').val(localStorage.getItem('13'));
+  $('#hour-14 .description').val(localStorage.getItem('14'));
+  $('#hour-15 .description').val(localStorage.getItem('15'));
+  $('#hour-16 .description').val(localStorage.getItem('16'));
+  $('#hour-17 .description').val(localStorage.getItem('17'));
+/*this allows the contents to stay on the page by pulling it from localstorage under the key number*/
+
 var timeClock = $('#currentDay')
 
-  timeClock.text(dayjs().format('dddd, MMMM D, YYYY'));
+  timeClock.text(dayjs().format('dddd, MMMM D, YYYY')); /*this adds the time to the to of the page*/
 });
